e52fca21d082fe2413a12624707ce5ee1d81eb64
neila <78797625+neila-a@users.noreply.github.com>
2022-02-17 12:48:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php
+$db = pg_connect("host=ec2-34-194-171-47.compute-1.amazonaws.com port=5432 dbname=dcgm55j0mip4mn user=qgrfqrjfxyecvc password=6b18a0960d68cf94859b8dd5c8d8cb2e7274b3bd7c8120821e39ecb7ecc6ea65");
 ?>
