bc62eeec7d3cd02eac217e526b94c43a3bae33b8
neila <78797625+neila-a@users.noreply.github.com>
2022-02-24 20:41:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,107 +2,77 @@
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-<title>LibearWork</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-    <!-- 可选的 Bootstrap 主题文件（一般不用引入） -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="/style.css">
-  </head>
-  <body>
-    <div class="container">
-	<div class="row clearfix">
-		<div class="col-md-12 column">
-			<img src="/img/help.webp" style="float:left;" class="mainPageButton" />
-<a href="/pages/devdoc">
-      <img src="/img/setting.webp" style="float:right;" class="mainPageButton" />
-</a>
-			<div class="jumbotron">
-				<h1 id="lbtitle">
-					LibearWork
-				</h1>
-			</div>
-      <form action="pages/workTableRegUser.php" class="mainPageForm">
-<br>
-<p>  加入组</p>
-<hr>
-<p>  </p><input name="groupID" type="text">
-<br>
-<p>  </p><button type="submit" class="mainPageSubmitBtn">加入</button>
-      <br>
-</form>
-<br>
-      <form action="pages/createNewGroup.php" class="mainPageForm">
-<br>
-<p>  创建组</p>
-<hr>
-<p>  </p><input name="groupID" type="text">
-<br>
-<p>  </p><button type="submit" class="mainPageSubmitBtn">创建</button>
-<br>
-<br>
-      </form>
-		</div>
-	</div>
-</div>
-<footer>LibearWork，©neila 2022-2022。本软件为开源软件。</footer>
-  </body>
+    <head>
+        <title>LibearWork</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+            integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"
+            crossorigin="anonymous"
+        />
+        <!-- 可选的 Bootstrap 主题文件（一般不用引入） -->
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css"
+            integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ"
+            crossorigin="anonymous"
+        />
+        <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+        <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+            integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
+            crossorigin="anonymous"
+        ></script>
+        <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="row clearfix">
+                <div class="col-md-12 column">
+                    <img
+                        src="/img/help.webp"
+                        style="float: left"
+                        class="mainPageButton"
+                    />
+                    <a href="/pages/devdoc/">
+                        <img
+                            src="/img/setting.webp"
+                            style="float: right"
+                            class="mainPageButton"
+                        />
+                    </a>
+                    <div class="jumbotron">
+                        <h1 id="lbtitle">LibearWork</h1>
+                    </div>
+                    <form action="pages/workTableRegUser.php" class="mainPageForm">
+                        <br />
+                        <p>    加入组</p>
+                        <hr />
+                        <p>    </p>
+                        <input name="groupID" type="text" />
+                        <br />
+                        <p>    </p>
+                        <button type="submit" class="mainPageSubmitBtn">加入</button>
+                        <br />
+                    </form>
+                    <br />
+                    <form action="pages/createNewGroup.php" class="mainPageForm">
+                        <br />
+                        <p>    创建组</p>
+                        <hr />
+                        <p>    </p>
+                        <input name="groupID" type="text" />
+                        <br />
+                        <p>    </p>
+                        <button type="submit" class="mainPageSubmitBtn">创建</button>
+                        <br />
+                        <br />
+                    </form>
+                </div>
+            </div>
+        </div>
+        <footer>LibearWork，©neila 2022-2022。本软件为开源软件。</footer>
+    </body>
 </html>
-<?php
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-<title>LibearWork</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-    <!-- 可选的 Bootstrap 主题文件（一般不用引入） -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="/style.css">
-  </head>
-  <body>
-    <div class="container">
-	<div class="row clearfix">
-		<div class="col-md-12 column">
-			<img src="/img/help.webp" style="float:left;" class="mainPageButton" />
-<a href="/pages/setting.php">
-      <img src="/img/setting.webp" style="float:right;" class="mainPageButton" />
-</a>
-			<div class="jumbotron">
-				<h1 id="lbtitle">
-					LibearWork
-				</h1>
-			</div>
-      <form action="pages/workTableRegUser.php" class="mainPageForm">
-<br>
-<p>  加入组</p>
-<hr>
-<p>  </p><input name="groupID" type="text">
-<br>
-<p>  </p><button type="submit" class="mainPageSubmitBtn">加入</button>
-      <br>
-</form>
-<br>
-      <form action="pages/createNewGroup.php" class="mainPageForm">
-<br>
-<p>  创建组</p>
-<hr>
-<p>  </p><input name="groupID" type="text">
-<br>
-<p>  </p><button type="submit" class="mainPageSubmitBtn">创建</button>
-<br>
-<br>
-      </form>
-		</div>
-	</div>
-</div>
-<footer>LibearWork，©neila 2022-2022。本软件为开源软件。</footer>
-  </body>
-</html> 
