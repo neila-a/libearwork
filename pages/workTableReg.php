5ff2368082ec191eb07ae4c3a573b665e747ec78
neila <78797625+neila-a@users.noreply.github.com>
2022-03-13 21:01:24 +0800

    Update workTableReg.php

--- pages/workTableReg.php
+++ pages/workTableReg.php
@@ -6,9 +6,18 @@
 <head>
 <?=$source?>
 </head>
-<body>
+<body style="text-align:center;">
     <img alt="140x140" src="../img/user.webp" class="img-circle" />
     <br />
     <h1 class="centerh1">请输入您的用户名</h1>
+    <form role="form" action="pages/newAccount.php">
+        <div class="form-group" style="display:none;">
+            <input type="text" name="type" value="<?php echo $_GET['type']; ?>" />
+        </div>
+		<div class="form-group">
+            <input type="text" class="form-control" name="groupID" />
+		</div>
+        <button type="submit" class="btn btn-default" style="background-color: #1e9fff;">创建</button>
+	</form>
 </body>
 </html>
