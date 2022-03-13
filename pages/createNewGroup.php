4dd784050f685414aee23d64aa5e097dd3cd324c
neila <78797625+neila-a@users.noreply.github.com>
2022-03-13 21:25:44 +0800

    Update createNewGroup.php

--- pages/createNewGroup.php
+++ pages/createNewGroup.php
@@ -1,7 +1,16 @@
 <?php
 require '../include.php';
-$dirname = "../data/" . $_GET["groupID"];
-mkdir($dirname,0777,true);
+$path = "../data/" . $_GET["groupID"];
+$mkdirconv = mkdir(iconv("UTF-8", "GBK", $path),0777,true); 
+if ($res){
+
+			echo "目录 $path 创建成功";
+
+		}else{
+
+			echo "目录 $path 创建失败";
+
+		}exit;
 $header = "location:workTableReg.php?type=admin&groupID=" . $_GET["groupID"]; 
 header($header);
 exit;
