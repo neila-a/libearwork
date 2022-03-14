e49120d4a796386ede8ec0f1f7aa959b2b468b35
neila <78797625+neila-a@users.noreply.github.com>
2022-03-14 19:34:48 +0800

    Update workTableReg.php

--- pages/workTableReg.php
+++ pages/workTableReg.php
@@ -12,13 +12,28 @@
     <h1 class="centerh1">请输入您的用户名</h1>
     <form role="form" action="pages/newAccount.php">
         <div class="form-group" style="display:none;">
-            <input type="text" name="type" value="<?php echo $_GET['type']; ?>" />
-	    <input type="text" name="groupID" value="<?php echo $_GET['groupID']; ?>" />
+            <input type="text" name="type" value="<?=$_GET['type']?>" />
+	    <input type="text" name="groupID" value="<?=$_GET['groupID']?>" />
         </div>
 		<div class="form-group">
             <input type="text" class="form-control" name="groupID" />
 		</div>
         <button type="submit" class="btn btn-default">创建</button>
-	</form>
+    </form>
+    <div name="info">
+	<p>用户类型：
+	    <?php 
+	        $type = $_GET['type'];
+		if ($type == "admin") {
+		    $r = "管理员";
+		} elseif ($type == "user") {
+		    $r = "普通用户";
+		} else {
+		    $r = "";
+		}
+		echo $r;
+	    ?> | 组ID：<?=$_GET['groupID']?>
+        </p>
+    </div>
 </body>
 </html>
